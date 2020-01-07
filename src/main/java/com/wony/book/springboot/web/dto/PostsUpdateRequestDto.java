3d485ede401c4d60d34999fdd382d044a435933b
HyewonDev <hyewon.dev@gmail.com>
2020-01-06 23:01:26 -0500

    page 212

---
+++ src/main/java/com/wony/book/springboot/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,17 @@
+package com.wony.book.springboot.web.dto;
+
+import com.wony.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+import lombok.Builder;
+
+@Getter
+public class PostsUpdateRequestDto {
+    private String title;
+    private String content;
+
+    @Builder
+    public PostsUpdateRequestDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
