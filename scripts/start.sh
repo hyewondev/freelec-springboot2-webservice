1c062b1b8b57783feb7749ff6834b48e17407ca7
HyewonDev <hyewon.dev@gmail.com>
2020-01-18 17:38:27 -0500

    edit classpath on start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -27,7 +27,6 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME running via profile=$IDLE_PROFILE"
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/
-    home/ec2-user/app/application-oauth.properties,/homeec2-user/app/application-real-db.properties \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/homeec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
